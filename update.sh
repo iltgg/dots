a08a9a35ab6def54e7c348fe33ca6be28264367b
ben <benlow999@gmail.com>
2024-01-04 23:40:26 -0500

    updates

---
+++ update.sh
@@ -0,0 +1,14 @@
+cp ~/.config/alacritty.toml ./
+cp -r ~/.config/hypr ./
+# cp -r ~/.config/kitty ./
+# cp -r ~/.config/neofetch ./
+# cp -r ~/.config/pipewire ./
+# cp -r ~/.config/swaylock ./
+cp -r ~/.config/waybar ./
+cp -r ~/.config/wofi ./
+# cp -r ~/.config/cava ./
+# cp -r ~/.config/i3 ./
+cp -r ~/.config/mako ./
+# cp -r ~/.config/nvim ./
+# cp -r ~/.config/polybar ./
+cp -r ~/.config/wlogout ./
