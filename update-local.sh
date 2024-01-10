870284a7e95c811ad852a877c4ea736423876ab0
ben <benlow999@gmail.com>
2024-01-09 21:36:59 -0500

    create update scripts

---
+++ update-local.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${RED}DANGER: This script doesn't backup any local configs!${NC}"
+read -p "confirm [Yy]: " -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp ./alacritty.toml ~/.config/
+    cp -r ./hypr ~/.config/
+    cp -r ./neofetch ~/.config/
+    # cp -r ./pipewire ~/.config/
+    cp -r ./swaylock ~/.config/
+    cp -r ./waybar ~/.config/
+    cp -r ./wofi ~/.config/
+    cp -r ./cava ~/.config/
+    cp -r ./mako ~/.config/
+    cp -r ./nvim ~/.config/
+    cp -r ./wlogout ~/.config/
+    cp -r ./swappy ~/.config/
+    echo "update complete"
+fi
+echo "update cancelled"
---
+++ update-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp ~/.config/alacritty.toml ./
+cp -r ~/.config/hypr ./
+cp -r ~/.config/neofetch ./
+# cp -r ~/.config/pipewire ./
+cp -r ~/.config/swaylock ./
+cp -r ~/.config/waybar ./
+cp -r ~/.config/wofi ./
+cp -r ~/.config/cava ./
+cp -r ~/.config/mako ./
+cp -r ~/.config/nvim ./
+cp -r ~/.config/wlogout ./
+cp -r ~/.config/swappy ./
