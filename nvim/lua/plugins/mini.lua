8d85eae72a5c266a82ca55b4f2af3e19bee899c6
Benjamin Low <benlow999@gmail.com>
2024-01-15 23:33:08 -0500

    fix file explorer launch

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
   event = 'VeryLazy',
   version = false,
   config = function()
-    require('mini.files').setup()
+    require('mini.files').setup { options = { use_as_default_explorer = false } }
     vim.keymap.set({ 'n' }, '<leader>o', _G.MiniFiles.open, { desc = '[O]pen Files' })
     vim.api.nvim_set_hl(0, 'MiniFilesNormal', { link = 'Normal' })
     vim.api.nvim_set_hl(0, 'MiniFilesBorder', { link = 'Normal' })
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,5 @@
 return {
   'stevearc/oil.nvim',
-  event = 'VeryLazy',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function(_, opts)
     require('oil').setup(opts)
