b5d4297052fc861f5f8407ad151ad82fbbee8ee0
ben <benlow999@gmail.com>
2024-01-17 22:54:26 -0500

    remove old plugins

--- nvim/lua/plugins/ultimate-autopair.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'altermo/ultimate-autopair.nvim',
-  enabled = false,
-  event = { 'InsertEnter', 'CmdlineEnter' },
-  branch = 'v0.6', -- recommended as each new version will have breaking changes
-  opts = {},
-}
