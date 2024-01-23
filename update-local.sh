d7a2750e033d277ed9c568ce57cd19272be6bd4c
Benjamin Low <benlow999@gmail.com>
2024-01-23 13:43:38 -0500

    set machine laptop when updating option

--- update-local.sh
+++ update-local.sh
@@ -5,11 +5,15 @@ NC='\033[0m'
 
 echo -e "${RED}DANGER: This script doesn't backup any local configs!${NC}"
 read -p "confirm [Yy]: " -r
-if [[ $REPLY =~ ^[Yy]$ ]];
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "[a]ll | [s]imple (excludes hyprland)"
     read -p "update type: " -r
     if [[ $REPLY =~ ^[a]$ ]]; then
+        read -p "Switch Hyprland configs to laptop setting? [Yy]: " -r
+        LAPTOP=$REPLY
+        if [[ $LAPTOP =~ ^[Yy]$ ]]; then
+            ./set-machine.sh l
+        fi
         # rm -r ~/.config/nvim
         cp ./alacritty.toml ~/.config/
         cp -r ./hypr ~/.config/
@@ -27,6 +31,9 @@ then
         cp ./.bashrc ~/
         cp ./.inputrc ~/
         cp ./.vimrc ~/
+        if [[ $LAPTOP =~ ^[Yy]$ ]]; then
+            ./set-machine.sh d
+        fi
         echo "update complete"
     elif [[ $REPLY =~ ^[s]$ ]]; then
         # rm -r ~/.config/nvim
