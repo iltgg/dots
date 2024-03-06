c41d529ecba9be80a0e6bb8d015b798f2f119307
ben <benlow999@gmail.com>
2024-03-05 22:36:05 -0500

    improve local update script usability

--- update-local.sh
+++ update-local.sh
@@ -4,15 +4,15 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "${RED}DANGER: This script doesn't backup any local configs!${NC}"
-read -p "confirm [Yy]: " -r
+read -p "confirm [yN]: " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    read -p "Update Hyprland? [Yy]: " -r
+    read -p "Switch configs to laptop setting? [yN]: " -r
+    LAPTOP=$REPLY
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        read -p "Switch Hyprland configs to laptop setting? [Yy]: " -r
-        LAPTOP=$REPLY
-    fi
-    if [[ $LAPTOP =~ ^[Yy]$ ]]; then
         ./set-machine.sh l
+    fi
+    read -p "Update Hyprland? [yN]: " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
         cp -r ./hypr ~/.config/
         echo "updated hyprland"
     fi
