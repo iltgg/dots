f11afa4d81a731d9f7e0a16315c6a74e5ddcba97
Benjamin Low <benlow999@gmail.com>
2024-07-17 14:16:09 -0400

    simpleboard full screen fix

--- hypr/bin/toggle_simpleboard.sh
+++ hypr/bin/toggle_simpleboard.sh
@@ -5,5 +5,6 @@ PROCESS="simpleboard"
 if pgrep -x "$PROCESS" > /dev/null; then
     pkill -f "$PROCESS"
 else
-    kitty -e simpleboard &
+    # kitty -e simpleboard &
+    hyprctl dispatch exec "[size 100% 100%; float; move 0% 0%; animation slide]" "kitty -e simpleboard"
 fi
