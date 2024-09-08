f6ac6cf5c3b7d9cda76abc8440627ec28d1a722f
Benjamin Low <benlow999@gmail.com>
2024-09-08 19:39:23 -0400

    delete old scripts

--- hypr/bin/cycle_zoom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-size=$(hyprctl -j getoption misc:cursor_zoom_factor | jq --raw-output '.float')
-size=${size%.*}
-
-if [[ $size -gt 1 ]]; then
-    if [[ $size -lt 6 ]]; then size=$((size + 1)); else size=2; fi
-    hyprctl keyword misc:cursor_zoom_factor $size
-fi
