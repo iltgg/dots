e40e8fce8403d76890c72bbf846e9cf95f01493c
Benjamin Low <benlow999@gmail.com>
2024-09-08 19:38:52 -0400

    add utility keybinds

--- hypr/bin/toggle_simpleboard.sh
+++ hypr/bin/toggle_simpleboard.sh
@@ -6,5 +6,5 @@ if pgrep -x "$PROCESS" > /dev/null; then
     pkill -f "$PROCESS"
 else
     # kitty -e simpleboard &
-    hyprctl dispatch exec "[size 100% 100%; float; move 0% 0%; animation slide]" "kitty -o font_size=40 -e simpleboard"
+    hyprctl dispatch exec "[size 100% 100%; float; move 0% 0%; animation slide]" "kitty -o font_size=40 -e simpleboard $1 $2"
 fi
---
+++ simpleboard/toggle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROCESS=$1
+BIN=$2
+
+if pgrep -x "$PROCESS" > /dev/null; then
+    pkill -f "$PROCESS"
+else
+    /bin/$BIN & disown
+fi
