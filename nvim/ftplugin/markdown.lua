c0dbbc51c40b7af07c4880bfe1629bebf4c9d315
Benjamin Low <benlow999@gmail.com>
2024-09-11 00:00:37 -0400

    add $ as text object

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -3,6 +3,11 @@ vim.keymap.set('n', '<leader>wc', 'g<C-g>', { desc = '[W]ord [C]ount (naive)' })
 vim.keymap.set({ 'i' }, '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u') -- Thank you Gilles Castel
 vim.keymap.set({ 'n' }, '<C-l>', '[s1z=`]')
 
+vim.api.nvim_set_keymap('o', 'i$', ':<C-u>normal! T$vt$<CR>', { noremap = true, silent = true }) -- allow action + in $ for latex
+vim.api.nvim_set_keymap('v', 'i$', 'T$ot$', { noremap = true })
+vim.api.nvim_set_keymap('o', 'a$', ':<C-u>normal! F$vf$<CR>', { noremap = true, silent = true }) -- allow action + around $ for latex
+vim.api.nvim_set_keymap('v', 'a$', 'F$of$', { noremap = true })
+
 vim.keymap.set('n', 'mi', 'ysiw*e', { remap = true })
 vim.keymap.set('n', 'mI', 'ds*', { remap = true })
 vim.keymap.set('v', 'mi', 'S*e', { remap = true })
